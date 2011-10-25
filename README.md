8fa23ceee20501c42bcedb5ecffdce8d78687d4d
AJ ONeal <coolaj86@gmail.com>
2011-10-25 11:54:44 -0600

    first working build of pakmanager

---
+++ README.md
@@ -0,0 +1,59 @@
+pakmanager
+===
+
+An example (and fully functional) package manager built on the `pakman` and `npm` APIs.
+
+Installation
+===
+
+If you haven't already set your NPM author info, now you should:
+
+    npm set init.author.name "Your Name"
+    npm set init.author.email "you@example.com"
+    npm set init.author.url "http://yourblog.com"
+
+    npm adduser
+
+And install pakmanager:
+
+    npm install -g pakmanager
+
+Usage
+===
+
+In short: run `pakmanager build` where `package.json` exists
+
+Create your project
+
+    PROJECT=~/Code/some-project
+    mkdir -p ${PROJECT}/lib
+    cd ${PROJECT}
+    touch lib/index.js
+    npm init
+
+Mark as private if need be by editing `package.json` and adding `"private": true,`
+
+Otherwise, publish your module
+
+    npm publish ./
+
+Test and build your module
+
+    pakmanager deps
+    pakmanager build
+
+CLI / API
+===
+
+    pakmanager deps       # list all dependencies
+    pakmanager build      # builds package.json.browserDependencies and package.json.main
+
+    rm -rf pakmanaged.js ./node_modules # clean old builds
+
+TODO
+===
+
+    pakmanager init       # creates / updates package.json
+    pakmanager install    # installs package.json.browserDependencies into ./node_modules
+    pakmanager clean      # rm -rf ./node_modules
+    pakmanager rebuild    # clean, build
---
+++ bin/pakmanager.js
@@ -0,0 +1,144 @@
+#!/usr/bin/env node
+(function () {
+  "use strict";
+
+  var fs = require('fs')
+    , pakmanager = require('pakmanager')
+    , args = process.argv.slice(2)
+    , action = args.shift()
+    , possibleAction
+    , params = []
+    , moduleNames = []
+    , actions = {
+          'help': 'help'
+        , 'h': 'help'
+        , 'build': 'build'
+        , 'b': 'add'
+        , 'c': 'add'
+        , 'deps': 'deps'
+        , 'add': 'add'
+        , 'a': 'add'
+        , 'remove': 'remove'
+        , 'rm': 'remove'
+        , 'r': 'remove'
+        , 'info': 'list'
+        , 'list': 'list'
+        , 'ls': 'list'
+        , 'u': 'update'
+        , 'up': 'update'
+        , 'update': 'update'
+        , 'l': 'list'
+        , 'refresh': 'refresh'
+      }
+    ;
+
+  function sortArgs() {
+    possibleAction = (action||'').replace(/^-{1,2}/, '');
+    if (!actions[possibleAction]) {
+      if (action) {
+        // we'll assume 'add' later an
+        args.unshift(action);
+        action = undefined;
+      }
+    }
+
+    args.forEach(function (arg) {
+      if (/^-{1,2}/.exec(arg)) {
+        params.push(arg.replace(/^-{1,2}/, ''));
+        return;
+      }
+
+      moduleNames.push(arg);
+    });
+  }
+
+  function tryPackageJson() {
+    fs.readFile('./package.json', function (err, data) {
+      var pkgModules
+        , pkgJson
+        ;
+
+      try {
+        pkgJson = data && JSON.parse(data.toString('utf8')) || {};
+      } catch(e) {
+        console.log('package.json:', e);
+        return;
+      }
+
+      pkgModules = (pkgJson.ender && pkgJson.ender.dependencies) || pkgJson.enderDependencies || pkgJson.dependencies || {};
+
+      if (Array.isArray(pkgModules)) {
+        moduleNames = pkgModules.concat(moduleNames);
+        pkgModules = {};
+      }
+
+      moduleNames = Object.keys(pkgModules).concat(moduleNames);
+
+      moduleNames.forEach(function (name, i) {
+        var semver = /\s*(=|>=|<=|~)\s*v?(\d+)(?:\.(\d+))?(?:\.(\d+))?\s*/.exec(pkgModules[name])
+          ;
+
+        //pkgModules[name] = pkgModules[name] || ">= 0.0.0"
+
+        if (semver && '=' === semver[1]) {
+          moduleNames[i] = name + '@' + semver[2] + '.' + semver[3] + '.' + semver[4];
+        }
+      });
+
+      if (moduleNames.length) {
+        doAction();
+        return;
+      }
+
+      if (err) {
+        console.error('Could not read package.json no modules were specified to add\n');
+      }
+
+      action = 'help';
+      doAction();
+    });
+  }
+
+  function doAction() {
+    if (!action && params.length) {
+      action = 'help';
+    }
+
+    action = (action||'').replace(/^-{1,2}/, '');
+
+    if (moduleNames.length) {
+      action = action || 'add';
+    }
+
+    action = actions[action] || 'help';
+
+    if ('help' === action) {
+      console.log('Usage: mend < add | list | update | remove > moduleName0 [, moduleName1, ...]');
+      console.log('Example: mend add jeesh ahr2 futures');
+      console.log('');
+      return;
+    }
+
+    // takeAction
+    pakmanager[action] && pakmanager[action](params, moduleNames, function () {
+      console.log('pakmanager', arguments);
+    });
+  }
+
+  // this is where we should get serious
+  function takeAction(action, params, moduleNames) {
+    console.log('takeAction', action, params, moduleNames);
+  }
+
+  /*
+  mend --build jeesh jQuery ahr2 -stupid reqwest futures  -g --build
+  --> add [ 'stupid', 'g', 'build' ] [ 'jeesh', 'jQuery', 'ahr2', 'reqwest', 'futures' ]
+  mend
+  mend --foo -bar
+  --> [help message is displayed]
+  */
+
+  sortArgs();
+  return doAction();
+  tryPackageJson();
+}());
